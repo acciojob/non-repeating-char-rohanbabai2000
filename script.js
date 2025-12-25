8f8bde4b5f3b9f891410c80f81cec1aa4fc8ac75
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-12-25 20:32:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let n=str.length;
+	let found = true;
+	for(let i=0;i<n;i++){
+		for(let j=0;j<n;j++){
+			if(i!==j && str[i]===str[j]){
+				found=false;
+				break;
+			}
+			if(!found){
+				return str[i];
+			}
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
