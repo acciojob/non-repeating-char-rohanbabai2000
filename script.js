bba7b1b471423a051309ec56610be32b81b883a4
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-12-25 20:43:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	let n=str.length;
-	let found = true;
+	
 	for(let i=0;i<n;++i){
+		let found = true;
 		for(let j=0;j<n;++j){
-			if(i!==j && str[i]===str[j]){
+			if( str[i]===str[j]){
 				found=false;
 				break;
 			}
